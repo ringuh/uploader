f1083b8b56c50cb8d9d71e7f70b2098bc0fe7e14
ringuh <anssi.vaakanainen@gmail.com>
2016-05-14 13:30:36 +0300

    functional

--- run.py
+++ run.py
@@ -1,18 +1,80 @@
 from config import conf
-print(conf)
+import paramiko, os, json, hashlib, pyperclip, threading, shutil
+from tkinter import Tk
 
-import paramiko
-k = paramiko.RSAKey.from_private_key_file(conf["key"])
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect( hostname =conf["server"], username=conf["user"], pkey=k )
+allowed = ["png", "jpg", "jpeg", "svg", "gif", "webm"]
 
-sftp = c.open_sftp()
-sftp.put('testi.key', '/home/picuploader/file.ext')
-c.close()
+script_path = os.path.dirname(os.path.realpath(__file__))
 
-# from ftplib import FTP_TLS
-# ftps = FTP_TLS('pienirinkula.com', "root", keyfile="pub_ssh.key")
-# ftps.login()
-# ftps.prot_p()
-# ftps.nlst()
+count = 0
+def Loop():
+	global script_path
+	global allowed
+	global conf
+
+
+	files = []
+	uploaded = []
+	for folder in conf["target"]:
+		# siirretään kuvat tähän kansioon uploadin jälkeen, varmistetaan, että kansio on olemassa
+		copy_path = os.path.join(folder, "uploaded")
+		try: os.stat(copy_path)
+		except: os.mkdir(copy_path)
+
+		# loopataan kaikki filut kansiosta, ignorataan alikansiot
+		for (dirpath, dirnames, filenames) in os.walk(folder):
+		    files.extend(filenames)
+		    break
+
+		# käydään läpi löydetyt hyväksytyllä päätteellä olevat tiedostot
+		for fn in files:
+			ext = fn.split(".")[-1]
+			if not ext in allowed:
+				continue
+			fpolku = os.path.join(folder, fn)
+			date_created = os.path.getmtime(fpolku)
+			# generoidaan tiedostoille toivottavasti uniikki nimi
+			m = hashlib.sha224(bytes("{}{}".format(fpolku, date_created), "utf-8")).hexdigest()
+
+			uploaded.append(dict(
+					path=fpolku,
+					date_created=date_created,
+					rename="{}.{}".format(m, ext),
+					move=copy_path,
+				))
+
+	#json.dump(new_files, f)
+
+	#f.close()
+
+
+
+	k = paramiko.RSAKey.from_private_key_file(
+			os.path.join(script_path, conf["key"])
+		)
+	c = paramiko.SSHClient()
+	c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	c.connect( hostname =conf["server"], username=conf["user"], pkey=k )
+
+	url = None
+
+	sftp = c.open_sftp()
+	for file in uploaded: # uploadataan
+		sftp.put(file["path"], '{}/{}'.format(conf["base"], file["rename"]))
+		url = "{}{}".format(conf["baseurl"], file["rename"])
+		print("Uploaded:"+file["rename"])
+		# ja siirretään toiseen kansioon
+		shutil.move(file["path"], os.path.join(file["move"], file["rename"]))
+	c.close()
+
+
+
+	if url: # kopioidaan viimeisen uploadatun kuvan osoite clipboardille
+		pyperclip.copy(url)
+
+	global count
+	# count += 1
+	if count < 2:
+		threading.Timer(15.0, Loop).start()
+
+Loop()
