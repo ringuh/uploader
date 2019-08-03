bc775cdf86f7fc0921ad65e13b943535a189c0f9
Anssi Vaakanainen <anssi.vaakanainen@gmail.com>
2019-08-03 03:36:31 +0300

    2019 rewrite with Win10 system Toast

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # uploader
-python script to be run in background.
+
 requires python and web server to upload the images to.
 
-will check selected folders every 15 seconds for new images and upload them on your server through SSH.
-will add the link to the uploaded picture on your clipboard.
+<img src="/static/toast_example.png"/>
 
-for windows create shortcut to Startup-folder for pythonw.exe.
-example shortcut target:
-C:\Users\<user>\AppData\Local\Programs\Python\Python35\pythonw.exe C:\Users\<user>\Documents\GitHub\uploader\run.py
+will check your selected folder every 5 seconds for new files and uploads them on your server through SSH.
+will prompt you with windows toast after upload to add the URL on your clipboard
 
+
+create virtualenv and install the requirements.txt
+create a shortcut to startup-folder where source is:
+"C:\Users\mypc\Envs\venv\Scripts\pythonw.exe C:\source_of_\uploader\run.py"
+
+ssh connection will requires using private-key without password
+
+conf = dict(
+	user="your_unix_login",
+	base="/var/www/<upload_folder>/",
+	server="your.server",
+	
+	key="private.key",
+	baseurl="https://your.server/",
+	target="D:\\<source_folder_on_your_windows_pc>",
+	toastExpiration=10000,
+)
--- run.py
+++ run.py
@@ -1,80 +1,133 @@
+import zroya, time, os, hashlib, paramiko, pyperclip, shutil, datetime
 from config import conf
-import paramiko, os, json, hashlib, pyperclip, threading, shutil
-from tkinter import Tk
 
-allowed = ["png", "jpg", "jpeg", "svg", "gif", "webm"]
+#def onClickHandler(notification_id):
+#def onActionHandler(notification_id, action_id):
+#def onDismissHandler(notification_id, reason):
+def GetStamp():
+	return str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
 
-script_path = os.path.dirname(os.path.realpath(__file__))
+f = open("loki.log", "w+")
+f.write("Started:"+GetStamp())
+f.close()
 
-count = 0
-def Loop():
-	global script_path
-	global allowed
-	global conf
+try:
+	images = ["png", "jpg", "jpeg", "gif", "svg", "gif"]
+	zroya.init("Uploader", "github.com/ringuh", "Uploader", "c", "v01")
+	freeLoop = True
+
+	def ErrorToast(e):
+		te = zroya.Template(zroya.TemplateType.ImageAndText4)
+		te.setFirstLine("Uploader Error:")
+		te.setSecondLine(str(e))
+		te.setImage("./prno.ico")
+		te.setExpiration(30000)
+		zroya.show(te)
+		# record the error message
+		f = open("loki.log", "w+")
+		f.write("Error:" + GetStamp()+"\n"+str(e))
+		f.close()
 
 
-	files = []
-	uploaded = []
-	for folder in conf["target"]:
-		# siirretään kuvat tähän kansioon uploadin jälkeen, varmistetaan, että kansio on olemassa
+	def Toast(url, path, filename):
+		def onActionHandler(notification_id, action_id):
+			
+			if action_id == 0:
+				#print("copy to clipboard:", url)
+				pyperclip.copy(url)
+
+		t = zroya.Template(zroya.TemplateType.ImageAndText4)
+		t.setFirstLine("Uploaded file:")
+		t.setSecondLine(filename)
+		
+		# set the potential picture as Toast Icon
+		t.setImage(path) if filename.split(".")[-1] in images else t.setImage("./static/prno.ico")
+		t.setAudio(zroya.Audio.Call6)
+			
+		
+		t.setExpiration(conf["toastExpiration"])
+		t.addAction("Copy URL")
+		t.addAction("Hide")
+		zroya.show(t, on_action=onActionHandler)
+
+
+
+
+	def Loop():
+		files = []
+		upload = []
+		freeLoop = False
+
+		folder = conf["target"]
+		# check if the folder you are suppose to backup uploaded files to exists and create it if necessary
 		copy_path = os.path.join(folder, "uploaded")
 		try: os.stat(copy_path)
 		except: os.mkdir(copy_path)
+		
 
-		# loopataan kaikki filut kansiosta, ignorataan alikansiot
+		# loop all files in folder, ignore other folders
 		for (dirpath, dirnames, filenames) in os.walk(folder):
 		    files.extend(filenames)
+		    # break ignores subdirectories
 		    break
+		#print(files)
 
-		# käydään läpi löydetyt hyväksytyllä päätteellä olevat tiedostot
 		for fn in files:
 			ext = fn.split(".")[-1]
-			if not ext in allowed:
-				continue
-			fpolku = os.path.join(folder, fn)
-			date_created = os.path.getmtime(fpolku)
-			# generoidaan tiedostoille toivottavasti uniikki nimi
-			m = hashlib.sha224(bytes("{}{}".format(fpolku, date_created), "utf-8")).hexdigest()
+			file_path = os.path.join(folder, fn)
+			date_created = os.path.getmtime(file_path)
+			# generate random unique name for the file
+			m = hashlib.sha224(bytes("{}{}".format(file_path, date_created), "utf-8")).hexdigest()
+			upload.append(
+				dict(
+					path=file_path,
+					rename="{}.{}".format(m, ext)
+				)
+			)
+		
+		if len(files) == 0:
+			return True
+		
+		# find the SSH key from your project
+		k = paramiko.RSAKey.from_private_key_file(
+					os.path.join(os.path.dirname(os.path.realpath(__file__)), conf["key"])
+				)
+		
+		c = paramiko.SSHClient()
+		c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		c.connect( hostname=conf["server"], username=conf["user"], pkey=k )
+		
+		
+		url = None
 
-			uploaded.append(dict(
-					path=fpolku,
-					date_created=date_created,
-					rename="{}.{}".format(m, ext),
-					move=copy_path,
-				))
+		sftp = c.open_sftp()
+		for file in upload: # loop through files to upload
+			sftp.put(file["path"], '{}/{}'.format(conf["base"], file["rename"]))
+			url = "{}{}".format(conf["baseurl"], file["rename"])
+			
+			# move the file to backup folder after upload
+			new_path = shutil.move(file["path"], os.path.join(copy_path, file["rename"]))
 
-	#json.dump(new_files, f)
+			# toast the URL for the last file or all files if its only a few of them
+			if len(upload) < 4 or file == upload[-1]:
+				Toast(url, new_path, file["rename"])
+		c.close()
+		return True
 
-	#f.close()
-
-
-
-	k = paramiko.RSAKey.from_private_key_file(
-			os.path.join(script_path, conf["key"])
-		)
-	c = paramiko.SSHClient()
-	c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	c.connect( hostname =conf["server"], username=conf["user"], pkey=k )
-
-	url = None
-
-	sftp = c.open_sftp()
-	for file in uploaded: # uploadataan
-		sftp.put(file["path"], '{}/{}'.format(conf["base"], file["rename"]))
-		url = "{}{}".format(conf["baseurl"], file["rename"])
-		print("Uploaded:"+file["rename"])
-		# ja siirretään toiseen kansioon
-		shutil.move(file["path"], os.path.join(file["move"], file["rename"]))
-	c.close()
+	while True: # keep the uploader active forever
+		try:
+			if freeLoop:				
+				freeLoop = Loop()
+				
+		except Exception as e:
+			ErrorToast(e)
+			raise e
+		
+		time.sleep(5)
 
 
+		
 
-	if url: # kopioidaan viimeisen uploadatun kuvan osoite clipboardille
-		pyperclip.copy(url)
-
-	global count
-	# count += 1
-	if count < 2:
-		threading.Timer(15.0, Loop).start()
-
-Loop()
+except Exception as e:
+	ErrorToast(str(e)+"\nshutting down")
+	raise e
